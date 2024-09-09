f918d0bbc4fbfb8b6d96a9386e1a9cee6460b27a
nwntaspap <nwntaspapmichael@gmail.com>
2024-09-09 20:22:50 +0300

    sidebar menu

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+
+function toggleSidebar() {
+  sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
+
+  closeAllSubMenu();
+}
+
+function toggleSubMenu(button) {
+  if (!button.nextElementSibling.classList.toggle("show")) {
+    closeAllSubMenu();
+  }
+  button.classList.toggle("rotate");
+
+  if (sidebar.classList.contains("close")) {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
+  }
+}
+
+function closeAllSubMenu() {
+  Array.from(sidebar.getElementsByClassName("show")).forEach((ul) => {
+    ul.classList.remove("show");
+    ul.previousElementSibling.classList.remove("rotate");
+  });
+}
